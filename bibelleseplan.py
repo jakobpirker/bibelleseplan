731290a27ebba19a8b3cff5abd86f1256681ecb0
Jakob <jakob.pirker@gmail.com>
2017-01-20 20:10:22 +0100

    - added argument parsing

--- bibelleseplan.py
+++ bibelleseplan.py
@@ -1,6 +1,7 @@
 import xlsxwriter
 import time
 import datetime
+import argparse
 from datetime import date
 import math
 
@@ -8,11 +9,23 @@ from definitions import BOOKS
 from section import Section
 import definitions as DEF
 
+# parse command-line-arguments
+parser = argparse.ArgumentParser(description="Create a bible reading plan, based on" 
+  " reading a certain amount of chapters a day, and creates an according excel file")
+parser.add_argument('-sec', nargs=1, type=str,
+  help = "list of the sections that should be used, e.g. '1 Mos - Mal, Mt - Offb' for book names see definitions.py")
+parser.add_argument('-ch', nargs=1, type=str,
+  help = "list of the numbers of chapters a day for a specific, according to the order given in -s, e.g. '1 3'")
+parser.add_argument('-d', nargs=1, type=str, help = "starting date, e.g. '17.1.2017'")
+parser.add_argument('-col', nargs=1, type=int, help = "number of columns the excel file should have")
+
+args = parser.parse_args()
+  
 # user definitions
-chapters_p_day = [1, 3]
-num_cols = 2
-section_str = "Mt - Offb, 1 Mos - Mal"
-s_date = "23.1.2017"
+chapters_p_day = [int(ch) for ch in args.ch[0].split()]
+num_cols = args.col[0]
+section_str = args.sec[0]
+s_date = args.d[0]
 
 # parse input arguments
 start = datetime.datetime.strptime(s_date, DEF.DATE_FORMAT).date()
--- run_blp.bat
+++ run_blp.bat
@@ -1,2 +1,2 @@
-python bibelleseplan.py
+python bibelleseplan.py -sec "1 Mos - Mal, Mt - Offb" -ch "3 1" -d "23.1.2017" -col 2
 pause
